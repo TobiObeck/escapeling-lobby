700db583c256338b4aac779043a3acafe2ab2b77
Tobi Obeck <13554426+TobiObeck@users.noreply.github.com>
2021-11-17 18:08:32 +0100

    working rooms and sending messages (history WIP)
    Co-authored-by: eliaszganning <eliaszganning@users.noreply.github.com>
    Co-authored-by: j.bajorath@outlook.de <j.bajorath@outlook.de>

--- server/user.py
+++ server/user.py
@@ -1,9 +1,7 @@
 class User:
-    def __init__(self, socket_id, name):
-        self._socket_id = socket_id # socket id from the client
+    def __init__(self, user_socket_id, name):
+        self._user_socket_id = user_socket_id # socket id from the client
         self._name = name # name entered by the user in the client UI
-        self._room_id = None 
 
-    def join_room(self, room):
-        self._room_id = room._id
-        room.add_player()
+    def get_id(self):
+        return self._user_socket_id
