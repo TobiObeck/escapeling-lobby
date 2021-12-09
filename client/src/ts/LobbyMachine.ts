48acdd13f517c76a0596afa1ee6404570ca0be3f
Tobi Obeck <13554426+TobiObeck@users.noreply.github.com>
2021-12-09 11:57:05 +0100

    refactors machine (child states) & user names are updated properly
    Co-authored-by: Janosch Bajorath <TheExplorer95@users.noreply.github.com>

--- client/src/ts/LobbyMachine.ts
+++ client/src/ts/LobbyMachine.ts
@@ -86,7 +86,9 @@ export const createLobbyMachine = (usernameInpValue: string, roomSelValue: strin
         isadmin: null
     }
 
-    const lobbyMachine = createMachine<LobbyContext, LobbyEvent>({ // LobbySchema,
+    // TODO update typescript types for machine
+    const lobbyMachine = createMachine<any, any>({ // LobbySchema,
+    // const lobbyMachine = createMachine<LobbyContext, LobbyEvent>({ // LobbySchema,
         id: 'lobby',
         context: initialContext,
         initial: 'startscreen',
@@ -126,6 +128,7 @@ export const createLobbyMachine = (usernameInpValue: string, roomSelValue: strin
             },
             connecting: {
                 entry: (ctx, _) => updateUiConnectLoading(),
+                // TODO instead of promise, rewrite with callback service
                 invoke: {
                     id: 'connecter',
                     src: (ctx, event) => {
@@ -144,25 +147,32 @@ export const createLobbyMachine = (usernameInpValue: string, roomSelValue: strin
                         });
                     },
                     onDone: {
-                        target: 'connected',
-                        actions: assign({ io: (ctx, event) => event.data })
+                        target: 'newconnected',
+                        actions: [
+                            assign({ io: (ctx, event) => event.data }),
+                            (ctx, event) => {
+                                ctx.io.emit('join', {
+                                    userid: ctx.io.id,
+                                    username: ctx.username,
+                                });
+                            }
+                        ]
                     }
                     // onError: {
                     //     target: 'errorscreen',
                     //     // actions: assign({ error: (context, event) => event.data })
                     // }             
                 },
-                exit: [
-                    (ctx, event) => {
-                        ctx.io.emit('join', {
-                            userid: ctx.io.id,
-                            username: ctx.username,
-                        });
-                    }
-                ],
             },
-    
-            connected:{
+            errorscreen: {
+                entry: () => {
+                    // TODO currently this state is never reached
+                    console.log('EROROR EROROR EROROR EROROR EROROR ')
+                    document.write('error lol')
+                }
+            },
+            newconnected:{
+                initial: 'waiting',
                 invoke: {
                     id: 'connecter',
                     src: (ctx, event) => (callback, onReceive) => {
@@ -181,141 +191,133 @@ export const createLobbyMachine = (usernameInpValue: string, roomSelValue: strin
                                 usernames: arg['usernames']
                             }
     
-                            callback({ type: 'connection-established', value: value })
+                            callback({ type: 'new-user-joined', value: value })
                         })
                     }
                 },
-                on: {
-                    'connection-established': {
-                        target: 'room',                    
-                        actions: [
-                            assign({
-                                chathistory: (ctx, event) => { 
-                                    return event.value.chathistory
-                                },
-                                usernames: (ctx, event) => { 
-                                    return event.value.usernames
-                                },
-                                isadmin: (ctx, event) => { 
-                                    return event.value.isadmin
-                                }
-                            }),
-                            () => {
-                                console.log('THIS IS ONLY LOGGED WHEN THE CURRENT USER JOIN');
-                                console.log('BUT NOT WHEN OTHERS JOIN, WHICH IS BAD');
-                            },
-                            (ctx, event) => {
-                                updateUisetInstructionText(event.value.isadmin, ctx.username)
+                states:{
+                    waiting:{
+                        on: {
+                            'new-user-joined': { // local user
+                                target: 'room',                    
+                                actions: [
+                                    assign({
+                                        chathistory: (ctx, event) => { 
+                                            return event.value.chathistory
+                                        },
+                                        usernames: (ctx, event) => { 
+                                            return event.value.usernames
+                                        },
+                                        isadmin: (ctx, event) => { 
+                                            return event.value.isadmin
+                                        }
+                                    }),
+                                    () => {
+                                        console.log('THIS IS ONLY LOGGED WHEN THE CURRENT USER JOIN');
+                                        console.log('BUT NOT WHEN OTHERS JOIN, WHICH IS BAD');
+                                    },
+                                    (ctx, event) => {
+                                        updateUiUsersInRoom(ctx.usernames),
+                                        updateUisetInstructionText(event.value.isadmin, ctx.username),
+                                        updateUiChatMessage(ctx.chathistory)
+                                    }
+                                ]
                             }
-                        ]
-                    }
-                }
-            },
-    
-            errorscreen: {
-                entry: () => {
-                    // TODO currently this state is never reached
-                    console.log('EROROR EROROR EROROR EROROR EROROR ')
-                    document.write('error lol')
-                }
-            },
-            room: {
-            //     always: [
-            //         { 
-            //             target: '#lobby.room.waiting-for-players',
-            //             cond: () => true,
-            //             actions: () => {
-            //                 alert('DA PASSIEEERT WAS!!')
-            //             }
-            //         }
-            //     ],
-            //     states:{
-            //         initial: 'room.waiting-for-players',
-            //         'waiting-for-players': {
-            //             actions: () => {
-            //                 alert('DA PASSIEEERT WAS!! 2222')
-            //             }
-            //             // cond: (ctx) => ctx.msg != null && ctx.msg !== '',
-            //         },
-            //         'ready-to-play':{
-            //             actions: () => {
-            //                 alert('DA PASSIEEERT WAS!! 3333')
-            //             }
-            //         }
-            //     },
-                entry: [
-                    (ctx, _) => updateUiShowRoom(),
-                    (ctx, _) => {
-                        console.log('CONTEXT chathistory', ctx.chathistory)
-                        updateUiChatMessage(ctx.chathistory),
-                        updateUiUsersInRoom(ctx.usernames)
-                    }                     
-                ],
-                invoke: {
-                    id: 'msghandler',
-                    src: (ctx, event) => (callback, onReceive) => {
-    
-                        ctx.io.on('broadcast-message', function(args: ChatPayload){
-                            // const time: string = args['time']
-                            // const username: string = args['username']
-                            // const msg: string = args['msg']
-                            // const messagePair = [time, username, msg]
-    
-                            if(args.msg != null && args.msg.length != 0){                            
-                                callback({ type: 'message-received', value: args })
-                            }
-                        })
-                    }
-                },
-                on: {
-                    'message-received': {
-                        actions: [
-                            assign({
-                                chathistory: (ctx, event) => {  
-                                    return [...ctx.chathistory, event.value]
-                                }
-                            }),
-                            (ctx, _) => {
-                                updateUiChatMessage(ctx.chathistory)
-                            }
+                        }
+                    },
+                    room:{
+                        entry: [
+                            (ctx, _) => updateUiShowRoom(),
                         ],
-                    },                          
-                    back: 'startscreen',
-                    'msg.change': {
-                        actions: [
-                            assign({
-                                msg: (ctx, event) => {                                
-                                    return event.value
-                                }
-                            })]
-                    },
-                    'send.msg': {
-                        cond: (ctx) => ctx.msg != null && ctx.msg !== '',
-                        actions: [
-                            'sendMessage',
-                            // clearing the context.msg value
-                            // is done manually by firing an input event
-                            updateUiClearChatMessageInput
-                        ]
-                    },
-                    'show.instructions': {
-                        actions: [
-                            (ctx, _) => {
-                                //check if enough people in room
-                                console.log('show instruction')
-                                updateUiShowInstructions()
+                        invoke: {
+                            id: 'msghandler',
+                            src: (ctx, event) => (callback, onReceive) => {            
+                                ctx.io.on('broadcast-message', function(args: ChatPayload){            
+                                    if(args.msg != null && args.msg.length != 0){                            
+                                        callback({ type: 'message-received', value: args })
+                                    }
+                                })
                             }
-                        ]
-                    }, 
-                    'collapse.instructions': {
-                        actions: [
-                            (ctx, _) => {
-                                console.log('machine')
-                                updateUiCollapseInstructions()
+                        },
+                        on: {
+                            'message-received': {
+                                actions: [
+                                    assign({
+                                        chathistory: (ctx, event) => {  
+                                            return [...ctx.chathistory, event.value]
+                                        }
+                                    }),
+                                    (ctx, _) => {
+                                        updateUiChatMessage(ctx.chathistory)
+                                    }
+                                ],
+                            },                          
+                            back: {
+                                target: '#lobby.startscreen',
+                                actions: [
+                                    assign({
+                                        username: usernameInpValue,
+                                        lobbyname: roomSelValue,
+                                        io: null,
+                                        msg: '',
+                                        roomId: null,
+                                        chathistory: [],
+                                        usernames: [],
+                                        isadmin: null
+                                    })
+                                ]
+                            },
+                            'msg.change': {
+                                actions: [
+                                    assign({
+                                        msg: (ctx, event) => {                                
+                                            return event.value
+                                        }
+                                    })]
+                            },
+                            'send.msg': {
+                                cond: (ctx) => ctx.msg != null && ctx.msg !== '',
+                                actions: [
+                                    'sendMessage',
+                                    // clearing the context.msg value
+                                    // is done manually by firing an input event
+                                    updateUiClearChatMessageInput
+                                ]
+                            },
+                            'show.instructions': {
+                                actions: [
+                                    (ctx, _) => {
+                                        //check if enough people in room
+                                        console.log('show instruction')
+                                        updateUiShowInstructions()
+                                    }
+                                ]
+                            }, 
+                            'collapse.instructions': {
+                                actions: [
+                                    (ctx, _) => {
+                                        console.log('machine')
+                                        updateUiCollapseInstructions()
+                                    }
+                                ]
+                            },
+                            'new-user-joined': { // remote user, external user joined
+                                actions: [
+                                    assign({
+                                        usernames: (ctx, event) => { 
+                                            return event.value.usernames
+                                        },
+                                    }),
+                                    (ctx, _) => updateUiUsersInRoom(ctx.usernames),
+                                    () => {
+                                        console.log('USER IS ALREADY IN ROOM! ACTION FIRED');
+                                    }
+                                ]
                             }
-                        ]
+                        }
+
                     }
-                },
+                }
             },
         },
     },
